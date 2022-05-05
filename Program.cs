221a039ff6ac010e86880f399414851bbe15735d
alvaroku <dual.alvaro.ku@gmail.com>
2022-05-04 23:02:18 -0500

    cambios del video numero 14 listos

--- Program.cs
+++ Program.cs
@@ -4,9 +4,23 @@ namespace Etapa1{
         static void Main(string[]args){
             Escuela MiEscuela = new Escuela("Platzi Academy",2012, TiposEscuela.Primaria,
                 pais:"Colombia",ciudad:"Bogota");
-     
+
+            Curso Curso1 = new Curso(){
+                Nombre="102"
+            };
+            Curso Curso2 = new Curso(){
+                Nombre="201"
+            };
+            Curso Curso3 = new Curso(){
+                Nombre="302"
+            };
+
+
             Console.WriteLine(MiEscuela);
-            
+            Console.WriteLine("============");
+            Console.WriteLine($"{Curso1.Nombre}, {Curso1.UniqueId}");
+            Console.WriteLine($"{Curso2.Nombre}, {Curso2.UniqueId}");
+            Console.WriteLine($"{Curso3.Nombre}, {Curso3.UniqueId}");
         }
     }
 }
