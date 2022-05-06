92095058a853099e0930592fdb582d5c54192738
alvaroku <dual.alvaro.ku@gmail.com>
2022-05-06 09:54:01 -0500

    cambios del video numero 16 listos

--- Program.cs
+++ Program.cs
@@ -2,25 +2,40 @@
 namespace Etapa1{
     class Program{
         static void Main(string[]args){
+
             Escuela MiEscuela = new Escuela("Platzi Academy",2012, TiposEscuela.Primaria,
                 pais:"Colombia",ciudad:"Bogota");
 
-            Curso Curso1 = new Curso(){
-                Nombre="102"
-            };
+            Curso[] arregloCursos = new Curso[3];
+
+            arregloCursos[0] = new Curso(){
+                                Nombre="102"
+                            };
+
             Curso Curso2 = new Curso(){
                 Nombre="201"
             };
-            Curso Curso3 = new Curso(){
-                Nombre="302"
-            };
+            arregloCursos[1] = Curso2;
 
+            arregloCursos[2] = new Curso{
+                                Nombre="302"
+                            };
 
             Console.WriteLine(MiEscuela);
             Console.WriteLine("============");
-            Console.WriteLine($"{Curso1.Nombre}, {Curso1.UniqueId}");
-            Console.WriteLine($"{Curso2.Nombre}, {Curso2.UniqueId}");
-            Console.WriteLine($"{Curso3.Nombre}, {Curso3.UniqueId}");
+            
+            ImprimirCursos(arregloCursos);
+
+        }
+
+        private static void ImprimirCursos(Curso[] arregloCursos)
+        {
+            int cont = 0;
+            while (cont<arregloCursos.Length)
+            {
+                Console.WriteLine($"Nombre: {arregloCursos[cont].Nombre}, Id: {arregloCursos[cont].UniqueId}");
+                cont++;
+            }
         }
     }
 }
