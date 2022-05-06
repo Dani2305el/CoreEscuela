356589e15d4a54d77bd50be751eaca0369fa84d7
alvaroku <dual.alvaro.ku@gmail.com>
2022-05-06 17:21:30 -0500

    cambios del video numero 28 listos

--- App/EscuelaEngine.cs
+++ App/EscuelaEngine.cs
@@ -1,4 +1,6 @@
+using System;
 using System.Collections.Generic;
+using System.Linq;
 using CoreEscuela.Entidades;
 
 namespace CoreEscuela.App
@@ -11,10 +13,55 @@ namespace CoreEscuela.App
         {
             
         }
-        public void Inicializar(){
+        public void Inicializar()
+        {
             Escuela = new Escuela("Platzi Academy", 2012, TiposEscuela.Primaria,
                 pais: "Colombia", ciudad: "Bogota");
-                
+
+            CargarCursos();
+            CargarAsignaturas();
+
+            foreach (Curso curso in Escuela.Cursos)
+            {
+                curso.Alumnos.AddRange(CargarAlumnos());
+            }
+            CargarEvaluaciones();
+        }
+
+        private void CargarEvaluaciones()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void CargarAsignaturas()
+        {
+            foreach (Curso curso in Escuela.Cursos)
+            {
+                List<Asignatura> listaAsignaturas = new List<Asignatura>(){
+                    new Asignatura{Nombre="Matemáticas"},
+                    new Asignatura{Nombre="Educación Física"},
+                    new Asignatura{Nombre="Castellano"},
+                    new Asignatura{Nombre="Ciencias Naturales"}
+                };
+                curso.Asignaturas.AddRange(listaAsignaturas);
+            }
+        }
+
+        private IEnumerable<Alumno> CargarAlumnos()
+        {
+            string[] nombre1 = { "Alba", "Felipa", "Eusebio", "Farid", "Donald", "Alvaro", "Nicolás" };
+            string[] apellido1 = { "Ruiz", "Sarmiento", "Uribe", "Maduro", "Trump", "Toledo", "Herrera" };
+            string[] nombre2 = { "Freddy", "Anabel", "Rick", "Murty", "Silvana", "Diomedes", "Nicomedes", "Teodoro" };
+
+            var listaAlumnos = from n1 in nombre1
+                                from n2 in nombre2
+                                from a1 in apellido1
+                                select new Alumno{Nombre = $"{n1} {n2} {a1}"};
+            return listaAlumnos;
+        }
+
+        private void CargarCursos()
+        {
             Escuela.Cursos = new List<Curso>(){
                         new Curso{Nombre = "101",Jornada = TiposJornada.Mañana},
                         new Curso{Nombre = "201",Jornada = TiposJornada.Mañana},
