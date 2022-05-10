efe14602733d9d49f8d31da7fa0eaef0bb148692
alvaroku <dual.alvaro.ku@gmail.com>
2022-05-10 10:56:52 -0500

    Curso de C# con .Net Core 2018 video 5

--- Program.cs
+++ Program.cs
@@ -19,14 +19,14 @@ namespace CoreEscuela
             Printer.DrawLine(20);
             Printer.DrawLine(20);
             Printer.WriteTitle("Pruebas de polimorfismo");
-            Alumno test = new Alumno{Nombre = "Alvaro"};
 
-            ObjetoEscuelaBase ob = test;
-            
+            Alumno test = new Alumno{Nombre = "Alvaro"};
             Printer.WriteTitle("Alumno");
             WriteLine($"Alumno: {test.Nombre}");
             WriteLine($"Alumno: {test.UniqueId}");
             WriteLine($"Alumno: {test.GetType()}");
+
+            ObjetoEscuelaBase ob = test;
             Printer.WriteTitle("ObjetoEscuela");
             WriteLine($"Alumno: {ob.Nombre}");
             WriteLine($"Alumno: {ob.UniqueId}");
@@ -37,6 +37,22 @@ namespace CoreEscuela
             WriteLine($"Alumno: {dummy.Nombre}");
             WriteLine($"Alumno: {dummy.UniqueId}");
             WriteLine($"Alumno: {dummy.GetType()}");
+
+            Evaluacion ev = new Evaluacion(){Nombre="ev mate",Nota=4.5f};
+            Printer.WriteTitle("Evaluacion");
+            WriteLine($"Evaluacion: {ev.Nombre}");
+            WriteLine($"Evaluacion: {ev.UniqueId}");
+            WriteLine($"Evaluacion: {ev.Nota}");
+            WriteLine($"Evaluacion: {ev.GetType()}");
+            
+            ob = ev;
+            Printer.WriteTitle("ObjetoEscuela");
+            WriteLine($"Evaluacion: {ob.Nombre}");
+            WriteLine($"Evaluacion: {ob.UniqueId}");
+            WriteLine($"Evaluacion: {ob.GetType()}");
+
+            test = (Alumno)(ObjetoEscuelaBase)ev;
+
         }
         private static void ImprimirCursosEscuela(Escuela escuela)
         {
